eda2b6b0ad59e5bcf187787b0c565fe17168e0ac
valeriocipolla <valeriocipolla92@gmail.com>
2022-03-21 09:08:28 +0000

    first two bullet points - done

--- budget.py
+++ budget.py
@@ -1,7 +1,22 @@
 class Category:
+  ledger = []
 
+  def __init__(self, category):
+    self.category = category
 
+  def deposit(self, amount, description=""):
+    self.ledger.append({"amount": amount, "description": description})
+
+  def withdraw(self, amount, description=""):
+    self.ledger.append({"amount": (amount * -1), "description": description})
 
 
 
 def create_spend_chart(categories):
+  return 1
+
+app = Category("food")
+app.deposit(100, "apples")
+app.deposit(2, "apples")
+app.withdraw(14, "disco")
+print(app.ledger)
